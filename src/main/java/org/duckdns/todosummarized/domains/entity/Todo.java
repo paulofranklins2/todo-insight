c0b8f11ab84b5e008077cbefd27a0cb892c85622
paulofranklins <paulofranklinc@gmail.com>
2026-01-08 16:30:21 -0800

    add Todo entity

---
+++ src/main/java/org/duckdns/todosummarized/domains/entity/Todo.java
@@ -0,0 +1,44 @@
+package org.duckdns.todosummarized.domains.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.duckdns.todosummarized.domains.enums.TaskPriority;
+import org.duckdns.todosummarized.domains.enums.TaskStatus;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String title;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private TaskPriority priority;
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+    private LocalDateTime dueDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    /**
+     * Automatically sets createdAt timestamp when a new TOOD is created.
+     */
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    /**
+     * Automatically updates the updatedAt timestamp when a TOOD is modified.
+     */
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
